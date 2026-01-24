a2d793b801ff250868418ac9f80b233089739519
Richard <richardwang1337@gmail.com>
2026-01-23 22:08:46 -0600

    Update AppDatabase.kt

--- app/src/main/java/GaitVision/com/data/AppDatabase.kt
+++ app/src/main/java/GaitVision/com/data/AppDatabase.kt
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase
 
 @Database(
     entities = [Patient::class, Video::class, GaitScore::class, AngleData::class],
-    version = 5,
+    version = 6,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
